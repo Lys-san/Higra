b9d5a640eb1b8421bab1d4fe5405ae560c2da620
Lys-san <lysandre.macke@gmail.com>
2024-07-09 11:33:21 +0200

    Rm tools folder.

--- tools/higraTools.cmake
+++
@@ -1,5 +0,0 @@
-function(REGISTER_PYTHON_MODULE_FILES PYTHON_FILES)
-    foreach (FILE ${PYTHON_FILES})
-        configure_file(${FILE} ${CMAKE_CURRENT_BINARY_DIR}/${FILE} COPYONLY)
-    endforeach ()
-endfunction(REGISTER_PYTHON_MODULE_FILES)
